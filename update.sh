6836f1e674212be7901314fc5739256ea42c46d8
Mandy Schoep <2277717+OhMyMndy@users.noreply.github.com>
2026-03-08 12:54:34 +0100

    chore: update to 4.2.605

--- update.sh
+++ update.sh
@@ -3,9 +3,8 @@
 set -e
 
 versions="$(xmlstarlet sel -t -v '/component/releases/release/@version' com.automattic.beeper.metainfo.xml)"
-download_url="$(curl -LsI -o /dev/null -w %{url_effective}  https://api.beeper.com/desktop/download/linux/x64/stable/com.automattic.beeper.desktop)"
-current_version="$(echo $download_url | grep -Eo '[0-9.]+[0-9]+')"
-
+download_url="$(curl -LsI -o /dev/null -w %{url_effective} https://api.beeper.com/desktop/download/linux/x64/stable/com.automattic.beeper.desktop)"
+current_version="$(echo $download_url | grep -Eo -m 1 '[0-9]+\.[0-9.]+')"
 if ! grep "$current_version" <<<"$versions" &>/dev/null; then
   echo "Updating to version $current_version"
   curl -SsL "$download_url" -o /tmp/beeper.appimage
@@ -15,7 +14,7 @@ if ! grep "$current_version" <<<"$versions" &>/dev/null; then
   date="$(date +"%Y-%m-%d")"
   xmlstarlet ed -L -i "/component/releases/release[1]" -t elem -n release -v "" \
     -i "/component/releases/release[1]" -t attr -n version -v "$current_version" \
-    -i "/component/releases/release[1]" -t attr -n date -v "$date"  com.automattic.beeper.metainfo.xml
+    -i "/component/releases/release[1]" -t attr -n date -v "$date" com.automattic.beeper.metainfo.xml
 
   yq e -i ".modules[0].sources[0].url = \"$download_url\"" com.automattic.beeper.yml
   yq e -i ".modules[0].sources[0].sha256 = \"$sha256\"" com.automattic.beeper.yml
